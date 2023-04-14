5e7cd54f745884d9d36af6d9cb89bcafb2e6c774
Nayef <nayefabdullah111@gmail.com>
2023-04-13 22:49:12 -0400

    some changes

--- SelectionSortTest.java
+++ SelectionSortTest.java
@@ -20,15 +20,15 @@ public SelectionSortTest() {
                 arr[0] = 8;
                 arr[1] = 9;
                 arr[2] = 7;
-                arr[3] = 10;
-                arr[4] = 2;
+                arr[3] = 12;
+                arr[4] = 1;
 
                 int[] Sortedarr = new int[5];
-                Sortedarr[0] = 2;
+                Sortedarr[0] = 1;
                 Sortedarr[1] = 7;
                 Sortedarr[2] = 8;
                 Sortedarr[3] = 9;
-                Sortedarr[4] = 10;
+                Sortedarr[4] = 12;
 
                 SelectionSort selectionSort = new SelectionSort();
                 /** add tests to check for this unit test **/
@@ -42,14 +42,14 @@ public SelectionSortTest() {
                 arr[0] = -8;
                 arr[1] = -9;
                 arr[2] = -7;
-                arr[3] = -10;
-                arr[4] = -2;
+                arr[3] = -12;
+                arr[4] = -1;
                 int[] Sortedarr = new int[5];
-                Sortedarr[0] = -10;
+                Sortedarr[0] = -12;
                 Sortedarr[1] = -9;
                 Sortedarr[2] = -8;
                 Sortedarr[3] = -7;
-                Sortedarr[4] = -2;
+                Sortedarr[4] = -1;
 
                 SelectionSort selectionSort = new SelectionSort();
                 /** add tests to check for this unit test **/
